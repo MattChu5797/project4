fb5a17cfbdc25a290ed3cb8cf85fc2426bfde2f2
Matthew Chu <MattChu@matthews-mbp-3.wireless.newschool.edu>
2016-10-28 15:35:19 -0400

    Remixed Project Code

--- scripts.js
+++ scripts.js
@@ -1,152 +1,151 @@
 $(document).ready(function(){
 
-$('body').css('background-image', 'url(http://s1.img.yan.vn/YanNews/2167221/201605/20160527-112530-3_600x276.gif)');
 
   alert("Welcome to 'The Ultimate How I Met Your Mother' Quiz. \n\n \
  You will be asked a series of questions to test your knowledge of the show. \n\n \
  Click 'OK' to start the quiz.");
 
 var bcolor = prompt("Let's start off easy. \n\n \
-Which name is not a character on the show?" ,"Ted, Barney, Raven or Lily").toLowerCase();
-if (bcolor === "ted" || bcolor === "barney" || bcolor === "raven" || bcolor === "lily")
-if (bcolor === "ted")
+Which name is a character on the show?" ,"Todd, Bernie, Lily or Raven").toLowerCase();
+if (bcolor === "todd" || bcolor === "bernie" || bcolor === "lily" || bcolor === "Raven")
+if (bcolor === "todd")
   alert("Wrong! You're almost as bad as the cockamouse!");
-else if (bcolor === "barney")
+else if (bcolor === "bernie")
   alert("Wrong! You're almost as bad as the cockamouse!");
-else if (bcolor === "raven")
-  alert("Correct! You've seen at least seen one episode!");
 else if (bcolor === "lily")
+  alert("Correct! You've seen at least seen one episode!");
+else if (bcolor === "raven")
   alert("Wrong! You're almost as bad as the cockamouse!");
 else
   alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
 
-var bcolor2 = prompt("Okay, let's continue. \n\n \
-What color were Ted's cowboy boots?" ,"blue, red, brown, or black").toLowerCase();
-  if (bcolor2 === "blue" || bcolor2 === "red" || bcolor2 === "brown" || bcolor2 === "black")
-  if (bcolor2 === "blue")
+var bcolor2 = prompt("Okay, let's move on. \n\n \
+What color were Ted's cowboy boots?" ,"light red, red, dark red, or burgundy").toLowerCase();
+  if (bcolor2 === "light red" || bcolor2 === "red" || bcolor2 === "dark red" || bcolor2 === "burgundy")
+  if (bcolor2 === "light red")
     alert("Wrong! I think you might be The Jinx!");
   else if (bcolor2 === "red")
     alert("Good job! You're starting to prove yourself kid.");
-  else if (bcolor2 === "brown")
+  else if (bcolor2 === "dark red")
     alert("Wrong! I think you might be The Jinx!");
-  else if (bcolor2 === "black")
+  else if (bcolor2 === "burgundy")
     alert("Wrong! I think you might be The Jinx!");
   else
     alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor3 = prompt("Here's another pretty easy one, cool? \n\n \
-  What color was the instrument that Ted stole for Robin?" ,"silver, pink, red, or blue").toLowerCase();
-    if (bcolor3 === "silver" || bcolor3 === "pink" || bcolor3 === "red" || bcolor3 === "blue")
-    if (bcolor3 === "silver")
+  var bcolor3 = prompt("Here's another pretty easy question  \n\n \
+  What color was the instrument that Ted stole for Robin?" ,"light blue, maybe blue, not blue, or blue").toLowerCase();
+    if (bcolor3 === "light blue" || bcolor3 === "maybe blue" || bcolor3 === "not blue" || bcolor3 === "blue")
+    if (bcolor3 === "light blue")
       alert("Wrong! You can do better than that!");
-    else if (bcolor3 === "pink")
+    else if (bcolor3 === "maybe blue")
       alert("Wrong! You can do better than that!");
-    else if (bcolor3 === "red")
+    else if (bcolor3 === "not blue")
       alert("Wrong! You can do better than that!");
     else if (bcolor3 === "blue")
       alert("Correct! You know you're stuff!");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor4 = prompt("Let's get a little tougher now. \n\n \
-  What was the name of the pub that the apartment was above?" ,"MacLaren's, McAaron's, MacLeason's, or MacKaren's").toLowerCase();
-    if (bcolor4 === "maclaren's" || bcolor4 === "mcaaron's" || bcolor4 === "macleason's" || bcolor4 === "mackaren's")
+  var bcolor4 = prompt("Almost half way there! \n\n \
+  What was the name of the pub that the apartment was above?" ,"MacLaren's, Mclaron's, McLaren's, or Mcloron's").toLowerCase();
+    if (bcolor4 === "maclaren's" || bcolor4 === "mclaron's" || bcolor4 === "mclaren's" || bcolor4 === "mcloron's")
     if (bcolor4 === "maclaren's")
       alert("Good job! Someone has Netflix.");
-    else if (bcolor4 === "mcaaron's")
+    else if (bcolor4 === "mclaron's")
       alert("Wrong! DAMMIT PATRICE!");
-    else if (bcolor4 === "macleason's")
+    else if (bcolor4 === "mclaren's")
       alert("Wrong! DAMMIT PATRICE!");
-    else if (bcolor4 === "mackaren's")
+    else if (bcolor4 === "mcloron's")
       alert("Wrong! DAMMIT PATRICE!");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor5 = prompt("Even tougher questions? Challenge accepted! \n\n \
-  What was on the tie that Barney was forced to wear?" ,"Rainbows, Teddy Bears, Ducks, or Hearts").toLowerCase();
-    if (bcolor5 === "rainbows" || bcolor5 === "teddy bears" || bcolor5 === "ducks" || bcolor5 === "hearts")
-    if (bcolor5 === "rainbows")
+  var bcolor5 = prompt("Why are you still taking this quiz... \n\n \
+  What was on the tie that Barney was forced to wear?" ,"Pigeons, Roosters, Ducks, or Flamingos").toLowerCase();
+    if (bcolor5 === "Pigeons" || bcolor5 === "roosters" || bcolor5 === "ducks" || bcolor5 === "Flamingos")
+    if (bcolor5 === "pigeons")
       alert("Nope. You might be The Sexless Inkeeper.");
-    else if (bcolor5 === "teddy bears")
+    else if (bcolor5 === "roosters")
       alert("Nope. You might be The Sexless Inkeeper.");
     else if (bcolor5 === "ducks")
       alert("Correct! No slaps for you!");
-    else if (bcolor5 === "Hearts")
+    else if (bcolor5 === "flamingos")
       alert("Nope. You might be The Sexless Inkeeper.");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor6 = prompt("Ready for an even tougher one? \n\n \
-  What was Ted's 'Slutty' Halloween crush dressed?" ,"Kitty, Nurse, Witch, or Pumpkin").toLowerCase();
-    if (bcolor6 === "kitty" || bcolor6 === "nurse" || bcolor6 === "witch" || bcolor6 === "pumpkin")
-    if (bcolor6 === "kitty")
+  var bcolor6 = prompt("Ready for another one? \n\n \
+  What was Ted's 'Slutty' Halloween crush dressed?" ,"Slut, Squash, Ghost, or Pumpkin").toLowerCase();
+    if (bcolor6 === "slut" || bcolor6 === "squash" || bcolor6 === "ghost" || bcolor6 === "pumpkin")
+    if (bcolor6 === "slut")
       alert("Not even close. You must be from New Jersey.");
-    else if (bcolor6 === "nurse")
+    else if (bcolor6 === "squash")
       alert("Not even close. You must be from New Jersey.");
-    else if (bcolor6 === "witch")
+    else if (bcolor6 === "ghost")
       alert("Not even close. You must be from New Jersey");
     else if (bcolor6 === "pumpkin")
       alert("Very good! High Five!");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor7 = prompt("You're still here? Congrats! \n\n \
-  What is Marshall's nickname?" ,"Lil'Cookie, Big Pudge, Sexy Man, or Big Fudge ").toLowerCase();
-    if (bcolor7 === "lil'cookie" || bcolor7 === "grand pudge" || bcolor7 === "sexy man" || bcolor7 === "big fudge")
-    if (bcolor7 === "lil'cookie")
+  var bcolor7 = prompt("You're still here? Can you not. \n\n \
+  What is Marshall's nickname?" ,"Big Cookie, Big Cake, Big Brownie, or Big Fudge ").toLowerCase();
+    if (bcolor7 === "big cookie" || bcolor7 === "big cake" || bcolor7 === "big brownie" || bcolor7 === "big fudge")
+    if (bcolor7 === "big cookie")
       alert("Are you kidding me? Are you Jed Mosley?");
-    else if (bcolor7 === "grand pudge")
+    else if (bcolor7 === "big cake")
       alert("Are you kidding me? Are you Jed Mosley?");
-    else if (bcolor7 === "sexy man")
+    else if (bcolor7 === "big brownie")
       alert("Are you kidding me? Are you Jed Mosley?");
     else if (bcolor7 === "big fudge")
       alert("Right! You might be Legen-'wait for it'...");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor8 = prompt("Almost there, only a few questions left! \n\n \
-  What was Ted's code word for smoking weed?" ,"Lighting a candle, Eating a sandwich, Eating cereal, or Lighting the Christmas Tree ").toLowerCase();
-    if (bcolor8 === "lighting a candle" || bcolor8 === "eating a sandwich" || bcolor8 === "eating cereal" || bcolor8 === "lighting the christmas tree")
-    if (bcolor8 === "lighting a candle")
+  var bcolor8 = prompt("Almost there this is taking forever! \n\n \
+  What was Ted's code word for smoking weed?" ,"Eating a hashbrown, Eating a sandwich, Eating grass, or eating Trees ").toLowerCase();
+    if (bcolor8 === "eating a hashbrown" || bcolor8 === "eating a sandwich" || bcolor8 === "eating grass" || bcolor8 === "eating trees")
+    if (bcolor8 === "eating a hashbrown")
       alert("Wrong. You're more embarassing than 'Let's Go To The Mall'");
     else if (bcolor8 === "eating a sandwich")
       alert("Good job! You're a pro!");
-    else if (bcolor8 === "eating cereal")
+    else if (bcolor8 === "eating grass")
       alert("Wrong. You're more embarassing than 'Let's Go To The Mall");
-    else if (bcolor8 === "lighting the christmas tree")
+    else if (bcolor8 === "eating trees")
       alert("Wrong. You're more embarassing than 'Let's Go To The Mall");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor9 = prompt("This one is pretty hard... you up for it? \n\n \
-  What was the name of Robin Sparkle's rocker-alter ego?" ,"Robin Danger, Robin Needles, Robin Daggers, or Robin Killer").toLowerCase();
-    if (bcolor9 === "robin danger" || bcolor9 === "robin needles" || bcolor9 === "robin daggers" || bcolor9 === "robin killer")
-    if (bcolor9 === "robin danger")
+  var bcolor9 = prompt("This one is pretty hard but like if you've never seen this show they're all kind of hard. \n\n \
+  What was the name of Robin Sparkle's rocker-alter ego?" ,"Robin Knife, Robin Digger, Robin Daggers, or Robin Savage").toLowerCase();
+    if (bcolor9 === "robin knife" || bcolor9 === "robin digger" || bcolor9 === "robin daggers" || bcolor9 === "robin savage")
+    if (bcolor9 === "robin knife")
       alert("Heck no! She should change the name of the song to P.S. I Hate You");
-    else if (bcolor9 === "robin needles")
+    else if (bcolor9 === "robin digger")
       alert("Heck no! She should change the name of the song to P.S. I Hate You");
     else if (bcolor9 === "robin daggers")
       alert("Good job! P.S. I Love You");
-    else if (bcolor9 === "robin killer")
+    else if (bcolor9 === "robin savage")
       alert("Heck no! She should change the name of the song to P.S. I Hate You");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
-  var bcolor10 = prompt("Final question! \n\n \
-  What does Barney do?" ,"Accounting, Realtor, Haha please, or Exotic Dancer ").toLowerCase();
-    if (bcolor10 === "accounting" || bcolor10 === "realtor" || bcolor10 === "haha please" || bcolor10 === "exotic dancer")
-    if (bcolor10 === "Accounting")
+  var bcolor10 = prompt("This is the last question. Finally! \n\n \
+  What does Barney do?" ,"Stuff, Everything, Haha please, or Some Stuff ").toLowerCase();
+    if (bcolor10 === "stuff" || bcolor10 === "everything" || bcolor10 === "haha please" || bcolor10 === "some stuff")
+    if (bcolor10 === "stuff")
       alert("Nope, you're not Legendary.");
-    else if (bcolor10 === "Realtor")
+    else if (bcolor10 === "everything")
       alert("Nope, you're not Legendary.");
     else if (bcolor10 === "haha please")
       alert("...dary! Legendary!");
-    else if (bcolor10 === "exotic dancer")
+    else if (bcolor10 === "some stuff")
       alert("Nope, you're not Legendary.");
     else
       alert("Sorry, that's not an option, silly. Please refresh and start again!");
 
     alert("You finished the quiz! Now go 'Suit Up!' and do something useful!");
-    self.location = "http://vignette4.wikia.nocookie.net/himym/images/0/0a/Marshall-Perfect-Cocktail.gif/revision/latest?cb=20120406192732"
+
 });
